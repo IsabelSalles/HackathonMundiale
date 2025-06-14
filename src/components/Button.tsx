405cdafee74a87251deff829313c18e83b3c7e1f
Isabel Salles <isabelsallessantos@gmail.com>
2025-06-14 17:13:08 -0300

    Estrutura inicial do projeto

---
+++ src/components/Button.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import React, { ButtonHTMLAttributes } from "react";
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {}
+
+export function Button({ children, ...props }: ButtonProps) {
+  return <button {...props}>{children}</button>;
+}
